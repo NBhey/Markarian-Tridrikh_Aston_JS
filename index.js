9b80791251ebef8e1f93997c38bc366f6f19a17f
Oganes <og.marckaryan@yandex.ru>
2025-02-16 20:11:08 +0300

    start make hw4

--- index.js
+++ index.js
@@ -1,255 +1,60 @@
-// Задание 1
+// Задание 1. Написать ответ - почему массивы в JS являются "неправильными" и совмещают в себе несколько структур данных? Какие ?
+// Массивы в JavaScript часто называют "неправильными" из-за того, что они отличаются от классических массивов в других языках программирования и фактически сочетают в себе свойства нескольких структур данных.
+// Почему "неправильные"?
+// Традиционные массивы (например, в C, C++, Java) обычно обладают следующими характеристиками:
+// 1) Фиксированный размер: Размер задается при создании и не меняется.
+// 2) Однородные типы:  Содержат элементы только одного типа данных.
+// 3) Непрерывное хранение в памяти: Элементы расположены в памяти последовательно.
 
-// Литеральный
-{
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
-  const workingObject = {
-    a: middleArray,
-  };
-  console.log(workingObject);
-}
-//  Функция-конструктор
-{
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
+// Задание 2. Привязать контекст к функции logger()
 
-  function WorkingObjectConstructor() {
-    this.a = middleArray;
+//Call
+{
+  function logger() {
+    console.log(`output only external context: ${this.item}`);
   }
 
-  const workingObject = new WorkingObjectConstructor();
+  const obj = { item: "some value" };
 
-  console.log(workingObject);
+  logger.call(obj);
 }
-// Через класс
+// Apply
 {
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
-
-  class WorkingObjectClass {
-    constructor() {
-      this.a = middleArray;
-    }
+  function logger() {
+    console.log(`output only external context: ${this.item}`);
   }
 
-  const workingObject = new WorkingObjectClass();
+  const obj = { item: "some value" };
 
-  console.log(workingObject);
-}
-//  Метод Object.create
-{
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
-
-  const workingObject = Object.create(null);
-  workingObject.a = middleArray;
-
-  console.log(`null`, workingObject);
+  logger.apply(obj);
 }
 
 {
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
-
-  const workingObject = Object.create({});
-  workingObject.a = middleArray;
-
-  console.log(`{}`, workingObject);
-}
-// new Object
-{
-  const innerObject = {};
-  const middleArray = [1, 2, 3, innerObject];
-
-  const workingObject = new Object();
-  workingObject.a = middleArray;
-
-  console.log(workingObject);
-}
-
-console.log("------------------Задани 2------------------");
-{
-  const innerObject = { value: 1 };
-  const middleArray = [1, 2, 3, innerObject];
-  const workingObject = {
-    a: middleArray,
-  };
-  // Spread поверхностная копия
-  {
-    const shallowCopyObjectSpread = { ...workingObject };
-
-    console.log("Оригинал:", workingObject);
-    console.log("Поверхностная копия (Spread):", shallowCopyObjectSpread);
-  }
-  // Object.assign поверхностная копия
-  {
-    const shallowCopyObjectAssign = Object.assign({}, workingObject);
-
-    console.log("Оригинал:", workingObject);
-    console.log("Поверхностная копия (Assign):", shallowCopyObjectAssign);
-  }
-  // Parse/stringify глубокая копия
-  {
-    const deepCopyObjectJSON = JSON.parse(JSON.stringify(workingObject));
-
-    console.log("Оригинал:", workingObject);
-    console.log("Глубокая копия (JSON):", deepCopyObjectJSON);
-  }
-}
-
-console.log("------------------Задани 3------------------");
-// Стрелочная функция
-{
-  const makeCounter = () => {
-    let count = 0;
-    return () => count++;
-  };
-  const counter = makeCounter();
-  console.log(counter);
-  console.log(counter());
-}
-//  Через объект
-{
-  function makeCounter() {
-    return {
-      count: 0,
-      increment() {
-        return this.count++;
-      },
-    };
+  function logger() {
+    console.log(`output only external context: ${this.item}`);
   }
 
-  const counter = makeCounter();
-  console.log(counter);
-  console.log(counter.increment());
-}
-//  Через класс
-{
-  class Counter {
-    constructor() {
-      this.count = 0;
-    }
+  const obj = { item: "some value" };
 
-    increment() {
-      return this.count++;
-    }
-  }
-
-  const makeCounter = () => new Counter();
-
-  const counter = makeCounter();
-  console.log(counter);
-  console.log(counter.increment());
-}
-// Замыкание
-{
-  function makeCounter(startFrom = 0) {
-    let count = startFrom;
-    return function () {
-      return count++;
-    };
-  }
-
-  const counter = makeCounter();
-  console.log(counter);
-  console.log(counter()); //0
+  const newLogger = logger.bind(obj);
+  newLogger();
 }
 
-console.log("------------------Доп задания------------------");
 
-// Задача на замыкание 1:
-{
-  function createIncrement() {
-    let value = 0;
+// 3.1 this: 
 
-    function increment() {
-      value += 1;
-      console.log(value);
-    }
 
-    const message = `Current value is ${value}`;
+const obj = {
+    a: 1,
+    e: (function () {
+        return () => {
+            console.log(this.a);
+        };
+    })(),
+};
 
-    function log() {
-      console.log(message);
-    }
+obj.e(); // undefined, здесь undefined т.к. в момент вызова самовызывающейся функции возвращает функцию, которая не является методом объекта в момент создания, 
+// а значит ссылается на глобальный объект window. В глобальном объекте window нет a, следовательно a = undefined, можно было бы исправить это добавив в начале var = a
+// 
 
-    return [increment, log];
-  }
-
-  const [increment, log] = createIncrement();
-
-  increment(); // 1
-  increment(); // 2
-  increment(); // 3
-
-  log(); //"Current value is 0"        // почему не 3? Потому что при инициализации переменной она запомнила, что value = 0
-}
-// Как должно быть
-{
-  function createIncrement() {
-    let value = 0;
-
-    function increment() {
-      value += 1;
-      console.log(value);
-    }
-
-    function log() {
-      const message = `Current value is ${value}`;
-      console.log(message);
-    }
-
-    return [increment, log];
-  }
-
-  const [increment, log] = createIncrement();
-
-  increment(); // 1
-  increment(); // 2
-  increment(); // 3
-
-  log(); //"Current value is 3"
-}
-// Задача на замыкание 2:
-{
-  let group = getGroup();
-
-  group[0](); // 10
-  group[5](); // 10
-  // потому что функция запомнила переменную i
-  // и в момент ее вызова она посмотрит на нее, а за счет того что цикл закончил,
-  // там уже будет 10
-  function getGroup() {
-    let students = [];
-    let i = 0;
-    while (i < 10) {
-      students[i] = function () {
-        console.log(i);
-      };
-      i++;
-    }
-
-    return students;
-  }
-}
-
-// Задача на замыкание 3:
-
-{
-  var globalVar = "global";
-  var outerVar = "outer";
-
-  function outerFunc(outerParam) {
-    function innerFunc(innerParam) {
-      console.log(globalVar, outerParam, innerParam); // guess,outer,inner
-    }
-    return innerFunc;
-  }
-
-  const x = outerFunc(outerVar);
-  outerVar = "outer-2";
-  globalVar = "guess";
-  x("inner");//guess, outer-2, inner
-  // разобрался почему outer будет а не outer-2, ответ оставил неправильным для себя
-} 
+obj.e.call({ a: 2 }); // undefined
