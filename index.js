b1d8f3914bb898338d408aad5ce9a2a98fbbabc2
Oganes <og.marckaryan@yandex.ru>
2025-02-17 19:46:11 +0300

    make task 3.3

--- index.js
+++ index.js
@@ -72,3 +72,32 @@ obj.e.call({ a: 2 }); // undefined
   obj.child.b(); // undefined, window
   obj.child.c(); // 10, child{...}
 }
+//  3.3 this:
+{
+  function foo() {
+    const x = 10;
+    return {
+      x: 20,
+      bar: () => {
+        console.log(this.x);
+      },
+      baz: function () {
+        console.log(this.x);
+      },
+    };
+  }
+
+  const obj1 = foo();
+  obj1.bar(); // ? undefined
+  obj1.baz(); // ? 20
+
+  const obj2 = foo.call({ x: 30 });
+
+  let y = obj2.bar;
+  let z = obj2.baz;
+  y(); // ? 30
+  z(); // ? undefined
+
+  obj2.bar(); //  ? 30
+  obj2.baz(); //  ? 20
+}
